116b861bf00cf1552d3cfa42c2055923d9005baa
edson-souza-morais <soued0311@gmail.com>
2018-02-07 18:10:51 +0100

    first commit

---
+++ GabrielGuesses.py
@@ -0,0 +1,123 @@
+import json
+from requests import get
+import os.path
+import sys
+
+nGramSize = 7
+letterPairJSON = {}
+
+def insertInDictionary(gender, letterPair):
+    global letterPairJSON
+    if letterPair in letterPairJSON[gender]:
+        letterPairJSON[gender][letterPair] = letterPairJSON[gender][letterPair] + 1
+    else :
+        letterPairJSON[gender][letterPair] = 1
+
+def downloadSpellchecker():
+    url = "https://dictionary.spellchecker.lu/output/lb_LU_morph.dic"
+    fileName = "lb_LU_morph.dic"
+    with open(fileName, "wb") as file:
+        response = get(url)
+        file.write(response.content)
+
+def generateJSON():
+    global letterPairJSON, nGramSize
+    masculineLetterPairs = {}
+    feminineLetterPairs = {}
+    neutralLetterPairs = {}
+    letterPairJSON["masculine"] = masculineLetterPairs
+    letterPairJSON["feminine"] = feminineLetterPairs
+    letterPairJSON["neutral"] = neutralLetterPairs
+    with open("lb_LU_morph.dic") as f:
+        for line in f:
+            gender = getGenderOfWord(line.rstrip())
+            if gender:
+                words = list(line.rstrip().split(" ")[0].split("/")[0].lower())
+                if len(words) < nGramSize:
+                    nGramSize = len(words)
+                for i in range(1,nGramSize+1):
+                    letterPair = "".join(words[-i:])
+                    insertInDictionary(gender, letterPair)
+    with open("letterPairJSON.json", 'w') as outputfile:
+        json.dump(letterPairJSON, outputfile, ensure_ascii=False)
+
+def getGenderOfWord(word):
+    try:
+        tsSplit = word.split("ts:")[1]
+        result = None
+        if tsSplit:
+            if tsSplit == "masculine_" or tsSplit == "masculine_singular":
+                result = "masculine"
+            elif tsSplit == "feminine_" or tsSplit == "feminine_singular":
+                result = "feminine"
+            elif tsSplit == "neutral_" or tsSplit == "neutral_singular":
+                result = "neutral"
+        return result
+    except IndexError:
+        return None
+
+def getNumberInJSON(gender, letterPair):
+    global letterPairJSON
+    if letterPair in letterPairJSON[gender]:
+        return letterPairJSON[gender][letterPair]
+    else:
+        return 0
+
+def tellGender(word):
+    global letterPairJSON, nGramSize
+    original = word
+    word = word.lower()
+    if "/" in word:
+        word = word.split("/")[0]
+    masculine = {}
+    feminine = {}
+    neutral = {}
+    result = {}
+    masculine["number"] = 0
+    masculine["percentage"] = 0
+    feminine["number"] = 0
+    feminine["percentage"] = 0
+    neutral["number"] = 0
+    neutral["percentage"] = 0
+    result["masculine"]=masculine
+    result["feminine"]=feminine
+    result["neutral"]=neutral
+    result["total"]=0
+    result["highest"]=[]
+    result["word"]=original
+    if len(word) < nGramSize:
+        nGramSize = len(word)
+    for i in range(nGramSize+1,1,-1):
+        letterPair = word[-i:]
+        result["masculine"]["number"] += getNumberInJSON("masculine", letterPair)
+        result["feminine"]["number"] += getNumberInJSON("feminine", letterPair)
+        result["neutral"]["number"] += getNumberInJSON("neutral",letterPair)
+        if result["masculine"]["number"] != 0 or result["feminine"]["number"] != 0 or result["neutral"]["number"] != 0:
+            break
+
+    result["total"] = result["masculine"]["number"] + result["feminine"]["number"] + result["neutral"]["number"]
+    for key in ["masculine","feminine","neutral"]:
+        if result[key]["number"] == 0 or result["total"] == 0:
+            result[key]["percentage"] = 0
+        else:
+            result[key]["percentage"] = result[key]["number"] / result["total"]
+    highest = max(result["masculine"]["number"], result["feminine"]["number"], result["neutral"]["number"])
+    for key in ["masculine","feminine","neutral"]:
+        if highest == result[key]["number"]:
+            result["highest"].append(key)
+    return result
+
+def __init__():
+    global letterPairJSON
+    if not os.path.isfile("lb_LU_morph.dic"):
+        downloadSpellchecker()
+    if not os.path.isfile("letterPairJSON.json"):
+        generateJSON()
+    if not letterPairJSON:
+        with open("letterPairJSON.json", 'r') as inputfile:
+            letterPairJSON=json.load(inputfile)
+
+__init__()
+
+if __name__ == "__main__":
+    print(tellGender(sys.argv[1]))
---
+++ README.md
@@ -0,0 +1,14 @@
+# Dependencies
+* python3
+* requests
+
+# Usage
+## Script
+python3 GabrielGuesses.py Polarbier
+## Module
+import GabrielGuesses
+print(gabrielGuesses.tellGender("Polarbier"))
+## Result
+{'masculine': {'number': 4, 'percentage': 0.6666666666666666}, 'feminine': {'number': 1, 'percentage': 0.16666666666666666}, 'neutral': {'number': 1, 'percentage': 0.16666666666666666}, 'total': 6, 'highest': ['masculine'], 'word': 'Polarbier'}
+
+
